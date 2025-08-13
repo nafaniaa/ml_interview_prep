8e272efcb324e1562d10a250b254f4d641306532
nafaniaa <tgurinovich2005@gmail.com>
2025-08-13 18:35:41 +0300

    Solved 1-10 Python tasks (basics)

---
+++ PythonPractice/13-08-25/task1-10.py
@@ -0,0 +1,61 @@
+#1 Функция, которая возвращает сумму двух чисел
+
+# def add(a, b):
+#     return a + b
+
+
+# print(add(1, 4))
+# print(add(896, 209187))
+
+#2 Вывод длины строки
+# text = "Python"
+# print(len(text))
+
+#3 Функция для определения четности
+# def is_even(n):
+#     if n % 2 == 0:
+#         return True
+#     else:
+#         return False
+    
+# print(is_even(4))
+# print(is_even(5))
+
+#4 Cтроку "hello" в верхний регистр
+
+# text = "hello"
+# print(text.upper())
+
+#5 Найти максимальное в списке
+
+# numbers = [1, 5, 3, 9, 2]
+# print(max(numbers))
+
+#6
+# hello = "Hello"
+# world = "World"
+# string = hello + ', ' + world + '!'
+# print(string)
+
+#7 Функция произведения 
+# def multiply(a, b):
+#     return a * b
+
+# print(multiply(2, 3))
+
+#8
+# string = "Python"
+
+# if 'y' in string:
+#     print(True)
+# else:
+#     print(False)
+
+#9
+# numbers = [1, 18, 2]
+# print(numbers[1])
+
+#10
+import math
+num = 3.7
+print(math.ceil(num))
